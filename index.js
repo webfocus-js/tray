b2ff039706f2d02f9068474dee7c551e04ff10e9
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2021-11-08 05:25:51 +0000

    Update ctray package

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const {join} = require("path")
 const component = module.exports = require('@webfocus/component')("System Actions","");
+let _tray = {};
 let _icon = join(__dirname, 'favicon.ico');
-let _tray = null;
 
-component.menu = [ { text: component.name, disabled: true } ];
-
-component.setIcon = path => _icon = path;
-component.setTitle = name => component.menu[0].text = name;
-component.showTray = _ => {
-    if( !_tray ){
-        try{
-            component.debug("Starting Ctray");
-            _tray = new (require("ctray"))(_icon, component.menu);
-            _tray.start();
-            component.hidden = true;
-        }
-        catch(e){
-            component.debug("CTray failed to start %O", e)
-            component.hidden = false;
-        }
-    }
+try{
+    let Tray = require("ctray");
+    _tray = new Tray(_icon);
 }
-component.closeTray = _ => _tray ? _tray.stop() : null;
-component.updateTray = _ => _tray ? _tray.update() : null;
+catch(e){
+    component.warn("tray not available %o", e);
+}
+
+_tray.menu = [ { text: component.name, disabled: true } ]
+
+component.setIcon = path => _tray.icon = path;
+component.setTitle = name => {
+    _tray.tooltip = name;
+    _tray.menu[0].text = name;
+}
+
+component.closeTray = _ => _tray ? _tray.close() : null;
 component.addAction = (name, cb) => {
     component.debug("Adding action %s", name);
     let path = name.replace(/\s+/g, '-').toLowerCase();
@@ -31,18 +28,8 @@ component.addAction = (name, cb) => {
         cb();
         res.redirect(req.headers.referer);
     });
-    component.menu.push({
-        path: path,
-        text: name, 
-        callback : () => {
-            _tray.update();
-            component.debug("Ctray callback %s", name);
-            try{
-                cb();
-            }
-            catch(e){
-                component.debug(e);
-            }
-        }
-    })
+    _tray.menu = Object.values(_tray.menu).concat({
+        text: name,
+        callback: cb
+    });
 }
